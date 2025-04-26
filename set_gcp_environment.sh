4257c510da5428de26f5cbbba4067ec9bc4e2c21
Stefan Bosak <stefan.bosak@gmail.com>
2025-04-26 07:23:47 +0000

    Updated GCP environment variable template

--- set_gcp_environment.sh
+++ set_gcp_environment.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
-export CLUSTER_NAME=
-export REGION=
+# organization attributes
+# gcloud organizations list
+# gcloud organizations describe ${ORGANIZATION_ID}
+export ORGANIZATION_NAME=
+export ORGANIZATION_ID=
+export ORGANIZATION_NUMBER=
+
+# project attributes
+# gcloud projects list
+# gcloud config get-value project
+# gcloud projects describe ${PROJECT_ID}
+export PROJECT_NAME=
 export PROJECT_ID=
 export PROJECT_NUMBER=
+
+# location attributes
+# gcloud compute regions list
+export REGION=
+
+# cluster attributes
+# gcloud container clusters list
+# gcloud container clusters describe ${CLUSTER_NAME}
+export CLUSTER_NAME=
+
+# configuration details
+# gcloud config list
 
 gcloud auth login
 gcloud config set project ${PROJECT_ID}

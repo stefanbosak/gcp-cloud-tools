88fe2ceecd202f4f5c7e360b589ba1b9d2bfe033
Stefan Bosak <stefan.bosak@gmail.com>
2025-04-30 07:43:32 +0000

    Added google repository within standalone installation

--- standalone-install.sh
+++ standalone-install.sh
@@ -194,6 +194,9 @@ else
 fi
 
 echo "Installing gcloud tools..."
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | \
+     tee -a /etc/apt/sources.list.d/google-cloud-sdk.list && curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | \
+     gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg && apt-get update -y && \
 apt-get install -y \
     google-cloud-cli=${GCLOUD_CLI_VERSION}-0 \
     google-cloud-cli-gke-gcloud-auth-plugin=${GCLOUD_CLI_VERSION}-0 \
